945a940fc44e46129ff4bee965697e1052fde054
oven425 <oven425@yahoo.com.tw>
2026-05-19 08:47:36 +0800

    update
    1. study maxpool

--- ConsoleApp_CNN/Program.cs
+++ ConsoleApp_CNN/Program.cs
@@ -39,12 +39,50 @@ for (int i = 0; i < count; i++)
         { 2, 0, 2 }, 
         { 1, 2, 1 } 
     };
-    FastSaveBmp(pixels, cols, rows, "org.bmp");
-    Filter(filter, pixels, out var dst);
-    FastSaveBmp(dst, cols, rows, "filter.bmp");
+    //FastSaveBmp(pixels, cols, rows, "org.bmp");
+    //Filter(filter, pixels, out var dst);
+    //FastSaveBmp(dst, cols, rows, "filter.bmp");
+    Filter_(filter, pixels.Select(x=>(double)x/255.0).ToArray(), out var dst_filter);
+    MaxPool(dst_filter, out var dst_pool);
 
-    //DrawConsoleColor(img);
+}
 
+void MaxPool(double[] src, out double[]dst, int width=26, int height=26)
+{
+    dst = new double[width/2 * height/2];
+    for(int y=0;y<height; y=y+2 )
+    {
+        for(int x = 0; x<width; x=x+2)
+        {
+            var s1 = src[y * width + x];
+            var s2 = src[y * width + x + 1];
+            var s3 = src[(y + 1) * width + x];
+            var s4 = src[(y + 1) * width + x + 1];
+            dst[(y/2) * (width/2) + x/2] = Math.Max(Math.Max(s1, s2), Math.Max(s3, s4));
+        }
+    }
+}
+
+void Filter_(double[,] filter, double[] src, out double[] dst, int width = 28, int height = 28)
+{
+    dst = new double[(width-2) * (height-2)];
+    int index = 0;
+    for (int y = 0; y < height - 2; y++)
+    {
+        for (int x = 0; x < width - 2; x++)
+        {
+            var sum = 0.0;
+            for (int fy = 0; fy < 3; fy++)
+            {
+                for (int fx = 0; fx < 3; fx++)
+                {
+                    sum = sum + src[(y + fy) * width + x + fx] * filter[fx, fy];
+                }
+            }
+            dst[index] = Math.Max(0.0, sum);
+            index++;
+        }
+    }
 }
 
 void Filter(double[,] filter, byte[] src, out byte[] dst, int width = 28, int height = 28)
@@ -55,21 +93,6 @@ void Filter(double[,] filter, byte[] src, out byte[] dst, int width = 28, int he
     {
         for (int x = 0; x < width-2; x++)
         {
-            //var a00 = src[y*width+x]* filter[0,0];
-            //var a01 = src[y * width + x+1] * filter[1, 0];
-            //var a02 = src[y * width + x + 2] * filter[2, 0];
-            //var a10 = src[(y+1) * width + x] * filter[0, 1];
-            //var a11 = src[(y + 1) * width + x + 1] * filter[1, 1];
-            //var a12 = src[(y + 1) * width + x + 2] * filter[2, 1];
-            //var a20 = src[(y + 2) * width + x] * filter[0, 2];
-            //var a21 = src[(y + 2) * width + x+1] * filter[1, 2];
-            //var a22 = src[(y + 2) * width + x+2] * filter[2, 2];
-
-            //var sum = a00 + a01 + a02 + a10 + a11 + a12 + a20 + a21 + a22;
-            //sum/= 9;
-            //dst[index] = (byte)Math.Clamp(sum, 0,255);
-            //index++;
-
             var sum = 0.0;
             for(int fy = 0; fy < 3; fy++)
             {
@@ -85,33 +108,6 @@ void Filter(double[,] filter, byte[] src, out byte[] dst, int width = 28, int he
         }
     }
 }
-
-void DrawConsoleColor(double[,] image)
-{
-    for (int y = 0; y < 28; y++)
-    {
-        for (int x = 0; x < 28; x++)
-        {
-            int gray = (int)(image[x, y] * 255);
-            // 使用 ANSI 色彩代碼設定背景色 \x1b[48;2;R;G;Bm
-            Console.Write($"\x1b[48;2;{gray};{gray};{gray}m  \x1b[0m");
-        }
-        Console.WriteLine();
-    }
-}
-
-double[,] NormalizeImage(byte[] pixels, int width=28, int height=28)
-{
-    double[,] img = new double[width, height];
-    for (int i = 0; i < pixels.Length; i++)
-    {
-        img[i / width, i % width] = pixels[i] / 255.0;
-    }
-    return img;
-}
-
-
-
 
 
 void FastSaveBmp(byte[] pixels, int width, int height, string fileName)
@@ -132,22 +128,3 @@ void FastSaveBmp(byte[] pixels, int width, int height, string fileName)
     bmp.UnlockBits(data);
     bmp.Save(fileName, ImageFormat.Bmp);
 }
-
-//void FastSaveBmp(byte[,] pixels, int width, int height, string fileName)
-//{
-//    Bitmap bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-//    BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
-
-//    byte[] rgbValues = new byte[data.Stride * height];
-//    for (int i = 0; i < pixels.Length; i++)
-//    {
-//        // 每個像素填三次 (B, G, R)
-//        //rgbValues[i * 3] = pixels[i];     // Blue
-//        //rgbValues[i * 3 + 1] = pixels[i]; // Green
-//        //rgbValues[i * 3 + 2] = pixels[i]; // Red
-//    }
-
-//    Marshal.Copy(rgbValues, 0, data.Scan0, rgbValues.Length);
-//    bmp.UnlockBits(data);
-//    bmp.Save(fileName, ImageFormat.Bmp);
-//}
