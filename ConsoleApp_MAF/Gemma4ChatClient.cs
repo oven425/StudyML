87c34bc013b7f068a2c6000127938fa4d40d78fc
oven425 <oven425@yahoo.com.tw>
2026-07-01 08:53:47 +0800

    update
    1. interagted gemma4 into MAF, can Text, but tool not work

---
+++ ConsoleApp_MAF/Gemma4ChatClient.cs
@@ -0,0 +1,149 @@
+﻿using LLama;
+using LLama.Common;
+using Microsoft.Extensions.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_MAF
+{
+    public class Gemma4ChatClient(string modelpath) : IChatClient
+    {
+        ModelParams? m_Parameters;
+        LLamaWeights? m_Weights;
+        LLamaContext? m_Context;
+        InteractiveExecutor? m_Executor;
+        InferenceParams? m_InferenceParams;
+
+        public void Dispose()
+        {
+            m_Context?.Dispose();
+            this.m_Weights?.Dispose();
+        }
+
+
+        async Task Init(ChatOptions? options)
+        {
+            if (m_Parameters == null)
+            {
+                var tools = "";
+                if (options != null)
+                {
+                    foreach (var oo in options.Tools)
+                    {
+
+                        if (oo is AIFunction aifun)
+                        {
+
+                            string schemaJson = JsonSerializer.Serialize(aifun.JsonSchema, new JsonSerializerOptions { WriteIndented = false });
+                            var tt = $"<|tool>declaration:{aifun.Name}<tool|>";
+                        }
+
+                        var tool = """
+                        <|tool>declaration:get_datetime{
+                          description: <|"|>取得現在的時間<|"|>,
+                          parameters: {          
+                          }
+                        }<tool|>
+                        """;
+                        tools = tool;
+                    }
+                }
+                m_SystemPrompt = $"""
+                <|turn>system
+                {options?.Instructions ?? ""}
+                {tools}
+                <turn|>
+                """;
+
+                this.m_Parameters = new ModelParams(modelpath)
+                {
+                    ContextSize = 81920,
+                    GpuLayerCount = 0,
+                };
+                this.m_Weights = await LLamaWeights.LoadFromFileAsync(this.m_Parameters);
+                this.m_Context = this.m_Weights.CreateContext(this.m_Parameters);
+                this.m_Executor = new InteractiveExecutor(this.m_Context);
+
+                this.m_InferenceParams = new InferenceParams()
+                {
+                    //SamplingPipeline = new DefaultSamplingPipeline()
+                    //{
+                    //    TopK = 40,
+                    //    TopP = 0.9F,
+                    //    Temperature = 0.1F
+                    //},
+                    MaxTokens = 81920,
+                    AntiPrompts = ["<turn|>"]
+                };
+
+
+            }
+        }
+
+        string m_SystemPrompt = "";
+        bool m_IsFirst = true;
+        async public Task<ChatResponse> GetResponseAsync(IEnumerable<ChatMessage> messages, ChatOptions? options = null, CancellationToken cancellationToken = default)
+        {
+            var contents = new List<AIContent>();
+            await Init(options);
+            var strb = new StringBuilder();
+            var lastmsg = messages.LastOrDefault();
+            if (lastmsg != null)
+            {
+                string prompt_user = $"""
+                <|turn>user
+                {lastmsg.Text}<turn|>
+                <|turn>model
+                """;
+                if (m_IsFirst)
+                {
+                    prompt_user = $"{m_SystemPrompt}\n{prompt_user}";
+                    m_IsFirst = false;
+                }
+
+                await foreach (var token in this.m_Executor.InferAsync(prompt_user, this.m_InferenceParams))
+                {
+                    strb.Append(token);
+                }
+            }
+            ChatResponse? response = null;
+            string toolCallPattern = @"<\|tool_call>(.*?)<tool_call\|>";
+            var resp_str = strb.ToString();
+            MatchCollection toolCallMatchs = Regex.Matches(resp_str, toolCallPattern, RegexOptions.Singleline);
+            if (toolCallMatchs.Count > 0)
+            {
+                FunctionCallContent[] ff = [];
+                    
+                response = new(new ChatMessage(ChatRole.Tool, ff));
+            }
+
+
+            response ??= new(new ChatMessage(ChatRole.Assistant, strb.ToString()));
+            return response;
+        }
+
+        public object? GetService(Type serviceType, object? serviceKey = null)
+        {
+            return this;
+        }
+
+        async public IAsyncEnumerable<ChatResponseUpdate> GetStreamingResponseAsync(IEnumerable<ChatMessage> messages, ChatOptions? options = null, CancellationToken cancellationToken = default)
+        {
+            await Init(options);
+
+
+            await foreach (var token in this.m_Executor.InferAsync("", this.m_InferenceParams))
+            {
+                yield return new ChatResponseUpdate(ChatRole.Assistant, token)
+                {
+                    FinishReason = ChatFinishReason.Stop,
+                };
+            }
+        }
+    }
+}
--- ConsoleApp_MAF/Program.cs
+++ ConsoleApp_MAF/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_MAF;
 using LLama;
 using LLama.Common;
 using Microsoft.Agents.AI;
@@ -7,7 +8,11 @@ using System.ComponentModel;
 using System.Reflection;
 using System.Text;
 using System.Text.Json;
-//Microsoft.Agents.AI.AgentFileSkill;
+using System.Text.RegularExpressions;
+using static LLama.Common.ChatHistory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
 Console.WriteLine("Hello, World!");
 string m_ModelPath = @"..\..\..\..\gguf_gemma4\gemma-4-E2B-it-Q4_K_M.gguf";
 
@@ -16,32 +21,30 @@ var tools = new AIFunction[]
     AIFunctionFactory.Create(get_datetime)
 };
 var ssss = tools[0].JsonSchema.ToString().Replace("\"", "<|\"|>");
-var option = new ChatOptions
+var option = new ChatOptions()
 {
-    Tools = tools
-};
-
-var aa = new Gemma4ChatClient(m_ModelPath);
-
-
-var res = await aa.GetResponseAsync("", option);
-var agent = aa.AsAIAgent(new ChatClientAgentOptions()
-{
-    Name ="assiant",
-    ChatOptions = new ChatOptions()
-    {
-        Instructions = "111",
-        Tools = 
+    Instructions = "你是個Windows助理,所有回答要有禮貌以及使用繁體中文",
+    Tools =
         [
             AIFunctionFactory.Create(get_datetime)
         ]
-    }
+};
+
+var gemma4client = new Gemma4ChatClient(m_ModelPath);
+var funcclient = gemma4client.AsBuilder().UseFunctionInvocation().Build();
+var aaresp = await funcclient.GetResponseAsync("現在幾點?", option);
+
+//var res = await gemma4client.GetResponseAsync("", option);
+var agent = funcclient.AsAIAgent(new ChatClientAgentOptions()
+{
+    Name ="assiant",
+    ChatOptions = option
 });
 
-var session = await agent.CreateSessionAsync("");
+var resp_agent = await agent.RunAsync("現在幾點?");
 
-[Description("讀取資料夾的內容")]
-string get_datetime(string folder)
+[Description("取得現在的時間")]
+string get_datetime()
 {
     var resp = JsonSerializer.Serialize(new ToolResponse()
     {
@@ -59,107 +62,3 @@ public class ToolResponse
     public string Data { set; get; } = string.Empty;
 }
 
-public class Gemma4ChatClient(string modelpath) : IChatClient
-{
-    ModelParams? m_Parameters;
-    LLamaWeights? m_Weights;
-    LLamaContext? m_Context;
-    InteractiveExecutor? m_Executor;
-    InferenceParams? m_InferenceParams;
-
-    public void Dispose()
-    {
-        m_Context?.Dispose();
-        this.m_Weights?.Dispose();
-    }
-
-
-    async Task Init(ChatOptions? options)
-    {
-        if (m_Parameters == null)
-        {
-            this.m_Parameters = new ModelParams(modelpath)
-            {
-                ContextSize = 81920,
-                GpuLayerCount = 0,
-            };
-            this.m_Weights = await LLamaWeights.LoadFromFileAsync(this.m_Parameters);
-            this.m_Context = this.m_Weights.CreateContext(this.m_Parameters);
-            this.m_Executor = new InteractiveExecutor(this.m_Context);
-
-            this.m_InferenceParams = new InferenceParams()
-            {
-                //SamplingPipeline = new DefaultSamplingPipeline()
-                //{
-                //    TopK = 40,
-                //    TopP = 0.9F,
-                //    Temperature = 0.1F
-                //},
-                MaxTokens = 81920,
-                AntiPrompts = ["<turn|>"]
-            };
-            
-            var tools = "";
-            if(options != null)
-            {
-                foreach(var oo in options.Tools)
-                {
-                    var tool = """
-                        <|tool>declaration:list_directory{
-                          description: <|"|><|"|>,
-                          parameters: {
-                            type: <<|"|>object<|"|>,
-                            properties: {
-                              dir_path: {
-                                type: <|"|>string<|"|>,
-                                description: <|"|>資料夾的絕對路徑<|"|>
-                              }
-                            },
-                            required: [<|"|>dir_path<|"|>]
-                          }
-                        }<tool|>
-                        """;
-                }
-            }
-            m_SystemPrompt = """
-                <|turn>system
-                你是個Windows助理,所有回答要有禮貌以及使用繁體中文
-                <turn|>
-                """;
-        }
-    }
-
-    string m_SystemPrompt = "";
-
-    async public Task<ChatResponse> GetResponseAsync(IEnumerable<ChatMessage> messages, ChatOptions? options = null, CancellationToken cancellationToken = default)
-    {
-        await Init(options);
-
-        var strb = new StringBuilder();
-        await foreach (var token in this.m_Executor.InferAsync("", this.m_InferenceParams))
-        {
-            strb.Append(token);
-        }
-        ChatResponse response = new(new ChatMessage(ChatRole.Assistant, strb.ToString()));
-        return response;
-    }
-
-    public object? GetService(Type serviceType, object? serviceKey = null)
-    {
-        return this;
-    }
-
-    async public IAsyncEnumerable<ChatResponseUpdate> GetStreamingResponseAsync(IEnumerable<ChatMessage> messages, ChatOptions? options = null, CancellationToken cancellationToken = default)
-    {
-        await Init(options);
-
-
-        await foreach (var token in this.m_Executor.InferAsync("", this.m_InferenceParams))
-        {
-            yield return new ChatResponseUpdate(ChatRole.Assistant,token)
-            {
-                FinishReason = ChatFinishReason.Stop,
-            };
-        }
-    }
-}
